0b5ee992e8fb0a1c83abeeb295e72a200663919f
Marena Brinkhurst <29718272+marenab@users.noreply.github.com>
2021-09-29 15:03:56 -0700

    title and chapter updates

--- config.js
+++ config.js
@@ -5,9 +5,9 @@ var config = {
     markerColor: '#3FB1CE',
     theme: 'light',
     use3dTerrain: true,
-    title: 'REVOcean arctic ROV = HACON',
-    subtitle: 'An Arctic expedition like no other = Hot Vents in an Ice-Covered Ocean',
-    byline: 'By REV Ocean and C4IR Ocean = led by NIVA and CAGE',
+    title: 'HACON',
+    subtitle: 'Hot Vents in an Ice-Covered Ocean',
+    byline: 'led by UiT (CAGE) and NIVA',
     footer: 'Thank you to Marena Brinkhurst of the Mapbox Community Team for helping us build and update this story as the expedition continues.',
     chapters: [
         {
@@ -16,7 +16,7 @@ var config = {
             hidden: false,
             title: 'What is the cruise?',
             image: '',
-            description: 'REV Ocean is building the world’s most advanced <a href="https://www.revocean.org/vessel/" target="_blank">research and expedition vessel</a>. Their first expedition will take the REV Ocean ROV (Remotely Operated Vehicle) on an expedition to the depths of the Arctic ice-shelf.<br><br><i>Scroll down to follow the ROV on its first expedition to the arctic depths.</i>',
+            description: 'Stefan B&#252;nz from the University of Tromso: Centre for Arctic Gas Hydrate, Environment and Climate and Eva Ramirez-Llodra, from the Norwegian Institute for Water Research, last visited the Aurora Hydrothermal Vents in 2019.<br><br>These vents were discovered in 2014 during the German AURORA cruise led by Prof Antje Boetius (AWI) on board Polarstern and were visited for a second time during the HACON19 cruise (see the 2019 cruise <a href="https://haconfrinatek.com/blog/" target="_blank">blog</a> and <a href="https://haconfrinatek.com/2020/01/20/hacon-cruise-report/" target="_blank">report</a>). However, until today, no ROV has been able to dive successfully to the vent field, at 4000 m depth under permanent ice cover.',
             location: {
                 center: { lon: -18.44294, lat: 79.46374 },
                 zoom: 2.5,
@@ -46,7 +46,7 @@ var config = {
             hidden: false,
             title: 'Who is going?',
             image: './images/teamondeck2.jpg',
-            description: '',
+            description: 'B&#252;nz and Ramirez-Llodra are leading a <a href="https://haconfrinatek.com/hacon2021/" target="_blank">team</a> of 17 scientists 82.5 degrees north to the Arctic in search for the Aurora Hydrothermal Vents. The team of scientists represent 13 different nationalities: German, Italian, French, Norwegian, New Zealand, Irish, Portuguese, American, Finnish, British, Canadian, Spanish, and Indian.',
             location: {
                 center: { lon: -5.18003, lat: 80.72944 },
                 zoom: 3.53,
@@ -100,7 +100,7 @@ var config = {
             hidden: false,
             title: 'ROV onboarding + mobilization',
             image: './images/_E5C5504.JPG',
-            description: '',
+            description: 'Now the crew is getting the ROV ready for its first expedition.',
             location: {
                 center: { lon: 13.47772, lat: 78.32703 },
                 zoom: 6.65,
